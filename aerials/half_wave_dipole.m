2345114dfd52c187a5306a411aa6fae412a85df1
Jack Hunt <jackmileshunt@gmail.com>
2024-09-13 21:07:54 +0100

    Update half_wave_dipole.m

--- aerials/half_wave_dipole.m
+++ aerials/half_wave_dipole.m
@@ -5,6 +5,8 @@ clc;
 
 %% Model.
 f = 3.5e6; % Hz
+f_min_plot = 3e6;
+f_max_plot = 4e6;
 height = 10; % Meters
 material = 'copper';
 wire_rad = 0.1; % Meters
@@ -23,7 +25,8 @@ title('Half Wave Dipole');
 %% Analyse.
 % Impedance.
 impedance_values = impedance(hwd, f);
-disp(['Antenna impedance at ' num2str(f / 1e6) ...
+disp(['Dipole with length ' num2str(length) ' meters' ...
+    ' has impedance at ' num2str(f / 1e6) ...
     ' MHz: ' num2str(impedance_values) ' ohms']);
 
 % Pattern.
@@ -33,4 +36,8 @@ title('Radiation Pattern for Half Wave Dipole');
 
 % VSWR.
 vswr = vswr(hwd, f);
-disp(['VSWR at ' num2str(f/1e6) ' MHz: ' num2str(vswr)]);
+disp(['VSWR at ' num2str(f / 1e6) ' MHz: ' num2str(vswr)]);
+
+% Impedance and VSWR over range.
+[f_range, z, swr] = compute_impedance_vswr(hwd, f_min_plot, f_max_plot);
+plot_impedance_vswr(f_range, z, swr, 'Impedance and VSWR');
